7516cefbda1911e90655c749e53f461653cc4b0b
mattyws@gmail.com <Kallaguer0031>
2019-05-24 11:21:57 -0300

    Change nominal to binary to get values if data was already binarazed

--- timeseries_nominal_to_binary.py
+++ timeseries_nominal_to_binary.py
@@ -53,10 +53,10 @@ def fill_missing_events(icustay_id, all_features, new_events_files_path):
     print("#### {} ####".format(icustay_id))
     if os.path.exists(new_events_files_path + '{}.csv'.format(icustay_id)):
         events = pd.read_csv(new_events_files_path + '{}.csv'.format(icustay_id))
+        if 'Unnamed: 0' in events.columns:
+            events = events.drop(columns=['Unnamed: 0'])
         events = events.fillna(0)
         if len(events.columns) != len(all_features):
-            if 'Unnamed: 0' in events.columns:
-                events = events.drop(columns=['Unnamed: 0'])
             zeros = np.zeros(len(events))
             features_not_in = all_features - set(events.columns)
             for itemid in features_not_in:
@@ -104,36 +104,3 @@ partial_fill_missing_events = partial(fill_missing_events, all_features=features
                                       new_events_files_path=new_events_files_path)
 with mp.Pool(processes=3) as pool:
     pool.map(partial_fill_missing_events, args)
-
-# exit()
-# for index, patient in dataset_csv.iterrows():
-#     print("===== {} =====".format(patient['icustay_id']))
-#     if os.path.exists(events_files_path + '{}.csv'.format(patient['icustay_id'])):
-#         # Get events and change nominal to binary
-#         events = pd.read_csv(events_files_path + '{}.csv'.format(patient['icustay_id']))
-#         if 'Unnamed: 0' in events.columns:
-#             events = events.drop(columns=['Unnamed: 0'])
-#         nominal_in_events = [itemid for itemid in categorical_features_chartevents if itemid in events.columns]
-#         for itemid in nominal_in_events:
-#             if itemid not in nominal_events.keys():
-#                 nominal_events[itemid] = set()
-#             nominal_events[itemid] |= set(events[itemid].dropna().unique())
-#         events = pd.get_dummies(events, columns=nominal_in_events, dummy_na=False)
-#         events.to_csv(new_events_files_path + '{}.csv'.format(patient['icustay_id']), index=False)
-#
-# print("================ Add missing events ================")
-# # Now add for each patient the binary columns for the values that doesn't appear
-# for index2, patient in dataset_csv.iterrows():
-#     print("===== {} =====".format(patient['icustay_id']))
-#     if os.path.exists(new_events_files_path + '{}.csv'.format(patient['icustay_id'])):
-#         events = pd.read_csv(new_events_files_path + '{}.csv'.format(patient['icustay_id']))
-#         if 'Unnamed: 0' in events.columns:
-#             events = events.drop(columns=['Unnamed: 0'])
-#         for itemid in nominal_events.keys():
-#             for value in nominal_events[itemid]:
-#                 if value is np.nan:
-#                     continue
-#                 if itemid + '_' + str(value) not in events.columns:
-#                     events.loc[:, itemid + '_' + str(value)] = pd.Series(np.zeros(len(events)), index=events.index)
-#         events = events.fillna(0)
-#         events.to_csv(new_events_files_path + '{}.csv'.format(patient['icustay_id']), index=False)
